322d14b2b0ef4a72b7b7885b13d2d073e11f0bb3
agelostsal <agelos.tsal@gmail.com>
2021-03-31 16:44:17 +0300

    ARGO-2798 AMS 1.0.8 release

--- version/version.go
+++ version/version.go
@@ -8,7 +8,7 @@ import (
 
 var (
 	// Release version of the service. Bump it up during new version release
-	Release = "1.0.7"
+	Release = "1.0.8"
 	// Commit hash provided during build
 	Commit = "Unknown"
 	// BuildTime provided during build
