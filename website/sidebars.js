f310d8034c3ceebe42837d4c272717c3825bb6d2
Tas-sos <tasosnumberten@yahoo.gr>
2021-08-05 11:33:41 +0300

    Added in sidebar.

--- website/sidebars.js
+++ website/sidebars.js
@@ -4,6 +4,7 @@ module.exports = {
     'Argo Messaging API': ['api_basic', 'api_errors'],
     'API Calls' : ['api_auth', 'api_users', 'api_projects', 'api_topics', 'api_subscriptions', 'api_metrics', 'api_schemas', 'api_version', 'api_registrations'],
     'Guides': ['publisher'],
+    'How to use': ['how_to_use'],
     'Frequent Questions': ['qa', 'qa_ruby']
   },
 };
