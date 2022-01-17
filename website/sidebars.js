e7050e7ea459829f18480b1bf9bd748f80ea06c0
Themis Zamani <themiszamani@gmail.com>
2022-01-17 16:49:24 +0200

    Update sidebars.js

--- website/sidebars.js
+++ website/sidebars.js
@@ -4,7 +4,7 @@ module.exports = {
     'General Concepts': ['overview', 'msg_backend', 'msg_flow', 'auth', 'projects_users'],
     'Argo Messaging API': ['api_basic', 'api_errors'],
     'API Calls' : ['api_auth', 'api_users', 'api_projects', 'api_topics', 'api_subscriptions', 'api_metrics', 'api_schemas', 'api_version', 'api_registrations'],
-    'Guides': ['publisher','subscriber_quide', 'subscriber-pull_quide','subscriber-push_quide'],
+    'Guides': ['publisher','subscriber_guide', 'subscriber-pull_guide','subscriber-push_guide'],
     'How to use': ['how_to_use', 'ams_push_worker'],
     'Frequent Questions': ['qa', 'qa_ruby']
   },
